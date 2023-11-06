4b861bf34696c8ff6c5f5092c64a1ed167491741
Alex Gama <alexgama1612@gmail.com>
2023-11-06 12:58:44 +0000

    Removed chat code
    Will add scroll view and rework this in the future

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -9,13 +9,7 @@ using System;
 
 public class Chat : MonoBehaviour
 {
-    public List<string> bubbleText;
-    public TMP_InputField chatText;
-
-    public float chatBubbleX = -246.00f; //-246
-    public float chatBubbleY = -112.00f; //-112
-
-    // Start is called before the first frame update
+   // Start is called before the first frame update
     void Start()
     {
         
@@ -24,30 +18,6 @@ public class Chat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (chatText != null)
-        {
-            bubbleText.Add(chatText.text);
-
-            if (bubbleText.Count == 1)
-            {
-
-                var textMessage = new GameObject();
-                var message = textMessage.AddComponent<Text>();
-                Debug.Log(bubbleText.Count);
-                //message.text = bubbleText.ElementAt(1);
-                textMessage.transform.position = new Vector2(chatBubbleX, chatBubbleY);
-                textMessage.tag = "Message " + bubbleText.Count;
-            }
-
-            else
-            {
-                //var textComponent = chatCanvas.AddComponent<Text>();
-                //textComponent.text = bubbleText.ElementAt(bubbleText.Count);
-                //int previousMessageCount = bubbleText.Count - 1;
-                //float oldTextYCoord = GameObject.FindGameObjectWithTag("Message " + previousMessageCount).transform.position.y;
-                //textComponent.transform.position = new Vector2(chatBubbleX, oldTextYCoord + 20);
-                //textComponent.tag = "Message " + bubbleText.Count;
-            }
-        }
+      
     }
 }
